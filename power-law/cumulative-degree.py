97a252b55398f3392f4e6532762a8248a52ac3e4
Lorenzo Barberis Canonico <lbarberiscanoni@Lorenzos-MacBook-Pro.local>
2018-10-05 16:42:20 -0400

    fixed histogram

--- power-law/cumulative-degree.py
+++ power-law/cumulative-degree.py
@@ -7,7 +7,9 @@ sparse_mat = scipy.io.mmread('as-22july06.mtx')
 
 G = nx.from_scipy_sparse_matrix(sparse_mat)
 
-results = powerlaw.Fit(G)
+#results = powerlaw.Fit(G)
 
-powerlaw.plot_cdf(data=results)
+#powerlaw.plot_cdf(data=results)
+powerlaw.plot_cdf(data=G)
+print("plotted")
 plt.show()
--- power-law/histogram.py
+++ power-law/histogram.py
@@ -15,12 +15,4 @@ plt.title("Degree rank plot")
 plt.ylabel("degree")
 plt.xlabel("rank")
 
-# draw graph in inset
-plt.axes([0.45,0.45,0.45,0.45])
-Gcc=sorted(nx.connected_component_subgraphs(G), key = len, reverse=True)[0]
-pos=nx.spring_layout(Gcc)
-plt.axis('off')
-nx.draw_networkx_nodes(Gcc,pos,node_size=20)
-nx.draw_networkx_edges(Gcc,pos,alpha=0.4)
-
 plt.savefig("degree_histogram.png")
